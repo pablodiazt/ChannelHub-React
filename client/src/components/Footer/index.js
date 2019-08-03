4af3bd2205f4186ac5894b9300ea588edfbb7b57
Pablo Diaz <pablodiaztr@gmail.com>
2019-08-03 18:27:29 -0500

    changes

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <header class="title">
+        <h1 class="title">Footer </h1>
+    </header>
+  );
+}
+
+
+export default Footer;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header class="title">
+        <h1 class="title">Navbar </h1>
+    </header>
+  );
+}
+
+
+export default Nav;
