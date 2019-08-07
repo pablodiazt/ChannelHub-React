bf7a6440f172a0aad8817c6233de075f42abeb8e
Pablo Diaz <pablodiaztr@gmail.com>
2019-08-06 19:04:52 -0500

    c

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.use(routes);
 
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/yourtimedb" { 
-  keepAlive: true, keepAliveInitialDelay: 300000 });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/yourtimedb");
 
 // This makes sure that any errors are logged if mongodb hangs
 db.on("error", function(error) {
