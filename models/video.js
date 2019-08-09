12729f2101f3b6921f2890d2a34280dcfee1c425
Antony Hernandez <wessoman@hotmail.com>
2019-08-09 05:43:13 -0500

    Wow Pablo did most of the work I had planned to do and I fixed the Schema

---
+++ models/video.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const videoSchema = new Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: false },
+  time: {type: [String], required: true },
+  synopsis: String,
+  date: { type: Date, default: Date.now }
+},
+{ timestamps: true },
+);
+
+const Video = mongoose.model("Video", videoSchema);
+
+module.exports = Video;
