f2d782540ba4966699ce7a5d9f70cd20eb644163
Pablo Diaz <pablodiaztr@gmail.com>
2019-08-10 09:10:30 -0500

    added video components

--- client/src/App.js
+++ client/src/App.js
@@ -4,132 +4,102 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Navbar from "./components/layout/Navbar";
+import SearchBar from "./components/component/SearchBar";
+import Videos from "./components/component/Videos";
+import Footer from "./components/layout/Footer";
 // import Footer from "./components/layout/Footer/Footer";    
 // import Header from "./components/layout/Footer/Header"; 
 
 class App extends Component {
-  render() {
-    return (
-        <div>
+    render() {
+        return (
             <div>
-                {/* <Header /> */}
-                <h2>Welcome to Your Time!</h2>
-                {/* <NavBar /> */}
+                <div>
+                    {/* <Header /> */}
+                    <h2>Welcome to Your Time!</h2>
+                    {/* <NavBar /> */}
+                </div>
+
+
+
+
+
+
+                {/* ----------------------------- NAVBAR */}
+
+
+                <Router>
+                    <Route exact path="/" component={Navbar} />
+                    <Route exact path="/" component={SearchBar} />
+                    <Route exact path="/" component={Videos} />
+                    <Route exact path="/" component={Footer} />
+                </Router>
+
+
+                {/* --------------------------------- VIDEOS */}
+                <div class="embed-responsive embed-responsive-1by1 w-25 p-3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                </div>
+
+                <div class="embed-responsive embed-responsive-1by1 w-25 p-3">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+                </div>
+
+
+                <footer class="page-footer font-small mdb-color darken-3 pt-4">
+
+                    {/* <!-- Footer Elements --> */}
+                    <div class="container">
+
+                        {/* <!--Grid row--> */}
+                        <div class="row d-flex justify-content-center">
+
+                            {/* <!--Grid column--> */}
+                            <div class="col-md-6">
+
+                                {/* <!-- Video --> */}
+                                <div class="embed-responsive embed-responsive-16by9 mb-4">
+                                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vlDzYIIOYmM"
+                                        allowfullscreen></iframe>
+                                </div>
+
+                            </div>
+                            {/* <!--Grid column--> */}
+
+                        </div>
+                        {/* <!--Grid row--> */}
+
+                    </div>
+
+                </footer>
+
+
+                {/* --------------------- */}
+
+
+
+                {/* ---------------------------------- COLLAPSE */}
+                <div class="pos-f-t">
+                    <div class="collapse" id="navbarToggleExternalContent">
+                        <div class="bg-dark p-4">
+                            <h4 class="text-white">Collapsed content</h4>
+                            <span class="text-muted">Toggleable via the navbar brand.</span>
+                        </div>
+                    </div>
+                    <nav class="navbar navbar-dark bg-dark">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    </nav>
+                </div>
+
+
+
             </div>
 
-        <Router>
-            
-            <Route exact path="/" component={Navbar} />
-        </Router>
 
-
-            
-
-
-{/* ----------------------------- NAVBAR */}
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search"></input>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-
-{/* --------------------------------- LINKS */}
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-  </div>
-</nav>
-
-<div class="embed-responsive embed-responsive-1by1 w-25 p-3">
-  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
-</div>
-
-<div class="embed-responsive embed-responsive-1by1 w-25 p-3">
-  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
-</div>
-
-{/* ---------------------------------- COLLAPSE */}
-<div class="pos-f-t">
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
-      <h4 class="text-white">Collapsed content</h4>
-      <span class="text-muted">Toggleable via the navbar brand.</span>
-    </div>
-  </div>
-  <nav class="navbar navbar-dark bg-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  </nav>
-</div>
-
-
-            
-        </div>
-        
-
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- client/src/components/layout/Navbar/index.js
+++ client/src/components/layout/Navbar/index.js
@@ -1,12 +1,61 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+// import { Link, NavLink } from "react-router-dom";
 import "./style.css";
+
+
 const NavBar = () => {
-  return (
-    <React.Fragment>
-      <nav className="navbar navbar-expand-lg navbar-light">
+    return (
+        <React.Fragment>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown link
+        </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+            {/* <nav className="navbar navbar-expand-lg navbar-light">
         <Link className="navbar-brand brand-title" to="#">
-          Home Page - Build your playlists
+          Home Pajjge - Build your playlists
         </Link>
         <button
           className="navbar-toggler"
@@ -33,9 +82,9 @@ const NavBar = () => {
             </li>
           </ul>
         </div>
-      </nav>
-    </React.Fragment>
-  );
+      </nav> */}
+        </React.Fragment>
+    );
 };
 
 export default NavBar;
