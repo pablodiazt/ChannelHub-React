9a0e747db1238c126410c3bef1d9cd9d431430cd
Pablo Diaz <pablodiaztr@gmail.com>
2019-08-12 10:00:14 -0500

    moved components to its place

---
+++ client/src/components/component/PlaylistName/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Something() {
+    return (
+        <React.Fragment>
+            {/* ----------- PLAYLIST NAME */}
+            <nav class="navbar navbar-light bg-light">
+                <span class="navbar-text">
+                    HTML Videos:
+                    </span>
+            </nav>
+        </React.Fragment>
+    );
+}
+
+export default Something;
