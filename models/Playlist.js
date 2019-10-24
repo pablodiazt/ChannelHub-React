0bb3bb035899ee92c549ba3c5f10d285c3140b04
mrowland-seu <mrowland@stedwards.edu>
2019-10-23 23:30:44 -0500

    add getThumbnailUrl funciton using noembed api

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const axios = require('axios');
 
 const PlaylistSchema = new Schema({
     title: { type: String, required: true },
@@ -9,16 +10,28 @@ const PlaylistSchema = new Schema({
     content: [{
 	title: String,
 	url: String,
-	backend: { type: String, enum: ['react-player', 'react-twitter-embed', 'external'], required: true },
+	backend: { type: String, enum: ['react-player', 'react-twitter-embed', 'noembed', 'external'], required: true },
 	isArchived: { type: Boolean, default: false },
 	thumbnailUrl: String
     }]
 });
 
 PlaylistSchema.pre('save', function(next) {
+    //get noembed endpoint list
     //collect thumbnail urls from noembed
     //evaluate backend type based on url 
-    next();
+    next()
+});
+
+function getThumbnailUrl(url) {
+    axios.get("https://noembed.com/embed?url="+url)
+	.then(function(response) {
+	    return {error: false, thumbnailUrl: response.data.thumbnail_url};
+	})
+	.catch(function(error) {
+	    console.log(error);
+	    return {error: true};
+	})
 }
 
 module.exports = mongoose.model("Playlist", PlaylistSchema); 
