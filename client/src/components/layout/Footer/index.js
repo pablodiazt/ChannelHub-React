333abb36e91687d7f6604c70956b10353d05c171
Pablo Diaz <pablodiaztr@gmail.com>
2019-10-26 22:43:26 -0500

    changes

---
+++ client/src/components/layout/Footer/index.js
@@ -0,0 +1,159 @@
+// dependencies:
+import React from "react";
+// ---- COMPONENTS / COMPONENT:
+// ---- files:
+import './style.css';
+
+
+function Footer() {
+    return (
+        <React.Fragment>
+
+
+
+            {/* <!-- Footer --> */}
+            <footer class="page-footer font-small mdb-color pt-4 myFooter">
+
+                {/* <!-- Footer Links --> */}
+                <div class="container text-center text-md-left">
+
+                    {/* <!-- Footer links --> */}
+                    <div class="row text-center text-md-left mt-3 pb-3">
+
+                        {/* <!-- Grid column --> */}
+                        <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Company name</h6>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                        </div>
+                        {/* <!-- Grid column --> */}
+
+                        <hr class="w-100 clearfix d-md-none"></hr>
+
+                        {/* <!-- Grid column --> */}
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                            <p>
+                                <a href="#!">Your Channels</a>
+                            </p>
+                            <p>
+                                <a href="#!">Your Playlists</a>
+                            </p>
+                            <p>
+                                <a href="#!">Favorite Channels</a>
+                            </p>
+                            <p>
+                                <a href="#!">Favorite Playlists</a>
+                            </p>
+                        </div>
+                        {/* <!-- Grid column --> */}
+
+                        <hr class="w-100 clearfix d-md-none"></hr>
+
+                        {/* <!-- Grid column --> */}
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+                            <p>
+                                <a href="#!">Your Account</a>
+                            </p>
+                            <p>
+                                <a href="#!">Followers</a>
+                            </p>
+                            <p>
+                                <a href="#!">Channels You Follow</a>
+                            </p>
+                            <p>
+                                <a href="#!">Support</a>
+                            </p>
+                        </div>
+
+                        {/* <!-- Grid column --> */}
+                        <hr class="w-100 clearfix d-md-none"></hr>
+
+                        {/* <!-- Grid column --> */}
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                            <p>
+                                <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                            <p>
+                                <i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+                            <p>
+                                <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                            <p>
+                                <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                        </div>
+                        {/* <!-- Grid column --> */}
+
+                    </div>
+                    {/* <!-- Footer links --> */}
+
+                    <hr></hr>
+
+                    {/* <!-- Grid row --> */}
+                    <div class="row d-flex align-items-center">
+
+
+
+
+
+
+                        {/* <!-- Grid column --> */}
+                        <div class="col-md-7 col-lg-8">
+
+                        </div>
+                        {/* <!-- Grid column --> */}
+
+
+
+
+
+
+                        {/* <!-- Grid column --> */}
+                        <div class="col-md-5 col-lg-4 ml-lg-0">
+
+                            {/* <!-- Social buttons --> */}
+                            <div class="text-center text-md-right">
+                                <ul class="list-unstyled list-inline">
+                                    <li class="list-inline-item">
+                                        <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                            <i class="fab fa-facebook-f"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                            <i class="fab fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                            <i class="fab fa-google-plus-g"></i>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item">
+                                        <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                            <i class="fab fa-linkedin-in"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                        {/* <!-- Grid column --> */}
+
+                    </div>
+                    {/* <!-- Grid row --> */}
+
+                </div>
+                {/* <!-- Footer Links --> */}
+
+            </footer>
+            {/* <!-- Footer --> */}
+
+
+
+
+
+        </React.Fragment>
+    );
+}
+
+export default Footer;
