8851315118fb69913c527858166b865c61cc866a
Pablo Diaz <pablodiaztr@gmail.com>
2019-10-28 00:31:20 -0500

    update user profile

---
+++ client/src/components/component/UserPortrait/index.js
@@ -0,0 +1,26 @@
+// dependencies:
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// ---- PAGES:
+import Login from "../../pages/Login";
+// ---- LAYOUT:
+// ---- COMPONENTS / COMPONENT:
+import SearchBar from "../../component/SearchBar";
+// ---- files:
+import "./style.css";
+
+
+class UserPortrait extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+
+
+            </React.Fragment >
+        );
+    }
+}
+
+
+export default UserPortrait;
