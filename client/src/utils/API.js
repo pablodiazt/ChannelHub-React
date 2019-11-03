2ac298deec7a64cfd4e57f33313b73e131bdc342
Pablo Diaz <pablodiaztr@gmail.com>
2019-11-03 10:35:50 -0600

    added api routes

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    // Gets all videos
+    getVideos: function () {
+        return axios.get("/api/videos");
+    },
+    // Gets the video with the given id
+    getVideo: function (id) {
+        return axios.get("/api/videos/" + id);
+    },
+    // Deletes the video with the given id
+    deleteVideo: function (id) {
+        return axios.delete("/api/videos/" + id);
+    },
+    // Saves a video to the database
+    saveVideo: function (videoData) {
+        return axios.post("/api/videos", videoData);
+    }
+};
