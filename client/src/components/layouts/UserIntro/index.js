a7ff74103d0a71085402d97553474aab2bbe760c
Pablo Diaz <pablodiaztr@gmail.com>
2019-11-02 21:03:50 -0500

    small routing changes

--- client/src/components/layouts/UserIntro/index.js
+++ client/src/components/layouts/UserIntro/index.js
@@ -3,9 +3,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 
 // ---- LAYOUTS:
-import ProfileInfo from "../ProfileInfo";
 // ---- COMPONENTS:
 import ProfilePicture from "../../components/ProfilePicture";
+import ProfileInfo from "../../components/ProfileInfo";
 import Bio from "../../components/Bio";
 import FollowedBy from "../../components/FollowedBy";
 // files:
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,12 +1,46 @@
 // dependencies:
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch, Link } from "react-router-dom";
 import jwt from 'jsonwebtoken';
 
 // ---- PAGES:
 import ChannelPlaylist from "./ChannelPlaylist";
+import SignUp from "./other/SignUp";
+import Login from "./other/Login";
+import UserProfile from "./other/UserProfile";
+// import Logout from "./components/pages/Logout";
 // ---- LAYOUTS:
 // ---- COMPONENTS:
+
+
+// function Home() {
+//     var token = localStorage.getItem("jsonwebtoken");
+//     if(token !== null) {
+//         var decodedToken = jwt.decode(token);
+//         var username = decodedToken.username;
+//         return (
+//             <Redirect to="/" />
+//             // <div>
+//             //     <h1> home</h1>
+//             //     <h3> Welcome, {username}!</h3>
+//             // </div>
+//         );
+//     }
+//     return (
+//         <Redirect to="/" />
+//         // <div>
+//         //     <h1> home</h1>
+//         //     <h3> Please login to continue.</h3>
+//         // </div>
+//     );
+// }
+
+function Logout() {
+    localStorage.removeItem("jsonwebtoken");
+    return (
+        <Redirect to='/' />
+    );
+}
 
 
 class Home extends Component {
@@ -18,6 +52,12 @@ class Home extends Component {
 
                 <Router>
                     <Route exact path="/" component={ChannelPlaylist} />
+
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/login" component={Login} />
+                    {/* <Route exact path="/logout" component={Logout} /> */}
+
+                    <Route exact path="/profile" component={UserProfile} />
                 </Router>
 
 
